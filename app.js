7b6c6b30dd17d9690bcc5ac7bbdd2aecc698e7fd
Zhao Zhong <zhao@zhaozhong.ca>
2021-11-08 01:03:12 -0700

    added various items

---
+++ app.js
@@ -0,0 +1,19 @@
+const auth = 'appid=***REMOVED***&'
+const apiLink = 'https://api.wolframalpha.com/v2/query?';
+const userInput = document.querySelector("#search");
+
+userInput.addEventListener('click', () => {
+  let textInput = '';
+  textInput = document.querySelector('#blank').value;
+  test(textInput);
+});
+
+async function test(textInput) {
+  try {
+    const res = await axios.get(`${apiLink}${auth}&input=${textInput}`);
+    console.log(res.data);
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
