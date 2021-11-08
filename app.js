d6a1b25853a3facbf6b95a7e71f183db169fc07c
Zhao Zhong <zhao@zhaozhong.ca>
2021-11-08 12:21:43 -0700

    push b4 the approval presentation

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
+//information in order to make API call -
 const auth = 'appid=5YQ6LG-GH3HQ42U69&'
 const apiLink = 'https://api.wolframalpha.com/v2/query?';
 const userInput = document.querySelector("#search");
 
-userInput.addEventListener('click', () => {
-  let textInput = '';
-  textInput = document.querySelector('#blank').value;
-  test(textInput);
-});
+//load the U.S. cities into the html as dropdown options in HTML
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-async function test(textInput) {
-  try {
-    const res = await axios.get(`${apiLink}${auth}&input=${textInput}`);
-    console.log(res.data);
-  }
-  catch (error) {
-    console.log(error);
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
 }
+
+//capture the user input
+// userInput.addEventListener('click', () => {
+//   let textInput = '';
+//   textInput = document.querySelector('#blank').value;
+//   test(textInput);
+// });
+
+
+
